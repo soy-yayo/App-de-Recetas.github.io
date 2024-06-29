8b714ae4faf9cf0d597c9a15c50b323120923963
yayo <yayomar211@gmail.com>
2024-06-28 18:10:29 -0600

    Se agregan recetas de prueba

--- js/index.js
+++ js/index.js
@@ -1,8 +1,28 @@
+let recetaPrueba1 = {
+    nombre: 'Tacos',
+    ingredientes: ['Tortilla', 'Carne', 'Salsa'],
+    preparacion: 'Calentar la tortilla, agregar la carne y la salsa'
+}
+let recetaPrueba2 = {
+    nombre: 'Enchiladas',
+    ingredientes: ['Tortilla', 'Pollo', 'Salsa'],
+    preparacion: 'Freir la tortilla, agregar el pollo, la salsa, queso y crema.'
+}
+let recetaPrueba3 = {
+    nombre: 'Pasta',
+    ingredientes: ['Pasta', 'Salsa', 'Queso'],
+    preparacion: 'Hervir la pasta, agregar la salsa y el queso.'
+}
+let recetas = [recetaPrueba1, recetaPrueba2, recetaPrueba3];
+let init = confirm('Bienvenido a la aplicacion de recetas, desea continuar?');
+
 function menu() {
     console.log('|------Menu------|\n1. Buscar Receta\n2. Agregar Receta\n3. Modificar Receta\n4. Eliminar Receta\n5. Salir\n|----------------|');
     op = parseInt(prompt('Ingrese una opcion: '));
     return op
 }
+let op = menu();
+
 if (init == true) {
     op = menu();
     switch (op) {
